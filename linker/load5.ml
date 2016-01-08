452d0429c81fa86fa5c6a31bb616b73511827ac5
pad <yoann.padioleau@gmail.com>
2016-01-08 11:05:14 -0800

    * linker/load5.ml: progress

--- linker/load5.ml
+++ linker/load5.ml
@@ -19,24 +19,36 @@ let process_ent ent h idfile =
   );
   lookup_ent ent h |> ignore
 
+(* visit entities to populate symbol table with wanted symbols 
+ * This is more complicated than in 5l because we can not rely
+ * on an ANAME or Operand.sym.
+ * boilerplate, could be auto generated.
+ *)
 
-(* visitors *)
-let rec visit_ximm x =
-  if true then raise Todo
+let visit_entities_line f x =
+  let rec ximm x =
+    if true then raise Todo
     (* process_ent e h idfile *)
-
-let rec visit_imm_or_ximm x =
-  if true then raise Todo
+  in
+  let rec imm_or_ximm x =
+    if true then raise Todo
+  in
+  let rec line x =
+    raise Todo
+  in
+  match x with
+  | Pseudo 
+  raise Todo
 
 
 
 (* load() performs a few things:
- * - split objects in code vs data,
- * - relocate branches,
+ * - load objects (of course),
+ * - split and concatenate in code vs data all objects,
+ * - relocate absolute jumps,
  * - "name" entities by assigning a unique name to every entities
  *   (handling private symbols),
- * - visit all entities to add in symbol table. This is more complicated
- *   than in 5l because we can not rely on an ANAME or Operand.sym.
+ * - visit all entities (defs and uses) and add them in symbol table
  *)
 let load entry xs =
 
@@ -48,7 +60,7 @@ let load entry xs =
   let pc = ref 0 in
   let idfile = ref 0 in
 
-  (* =~ T.lookup (entry, T.Public) None h *)
+  (* =~ T.lookup (entry, T.Public) None h |> ignore *)
   Hashtbl.add h (entry, T.Public) { T.section = T.SXref; signature = None; };
 
   xs |> List.iter (fun file ->
@@ -60,26 +72,34 @@ let load entry xs =
     let (prog, _srcfile) = Object_code5.load file in
 
     prog |> List.iter (fun (p, line) ->
+      visit_entities_line (fun ent ->
+        process_ent ent h !idfile;
+      );
       match p with
       | Pseudo pseudo ->
           (match pseudo with
           | TEXT (ent, attrs, size) ->
-              process_ent ent h !idfile;
+              (* less: set curtext *)
+              let v = lookup_ent ent h in
+              (match v.T.section with
+              | T.SXref -> v.T.section <- T.SText !pc;
+              | _ -> error (spf "redefinition of %s" ent.name)
+              );
+              (* less: adjust autosize? *)
               code |> Common.push (T5.TEXT (ent, attrs, size), line);
               incr pc;
           | WORD v ->
-              visit_imm_or_ximm v;
               code |> Common.push (T5.WORD v, line);
               incr pc;
             
           | GLOBL (ent, attrs, size) -> 
-              process_ent ent h !idfile;
-            raise Todo
+              let v = lookup_ent ent h in
+              (match v.T.section with
+              | T.SXref -> v.T.section <- T.SBss size;
+              | _ -> error (spf "redefinition of %s" ent.name)
+              );
           | DATA (ent, offset, size, v) -> 
-              process_ent ent h !idfile;
-              visit_imm_or_ximm v;
               data |> Common.push (T5.DATA (ent, offset, size, v))
-                                    
           )
 
       | Instr (inst, cond) ->
@@ -98,13 +118,11 @@ let load entry xs =
             | _ -> inst
           in
           code |> Common.push (T5.I (inst, cond), line);
-          incr pc
+          incr pc;
 
       | LabelDef _ -> error (spf "label definition in object")
-      | LineDirective _ ->
-        (* todo: return also, but for now stripped info *)
-        ()
-
+      (* todo: return also, but for now stripped info *)
+      | LineDirective _ -> ()
     );
   );
   Array.of_list (List.rev !code), List.rev !data, h

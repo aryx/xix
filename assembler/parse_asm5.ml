7ea7d160635ffdede1c0918dc5838bc8590377f6
pad <yoann.padioleau@gmail.com>
2016-01-14 10:21:55 -0800

    * assembler/parse_asm5.ml: better error report

--- assembler/parse_asm5.ml
+++ assembler/parse_asm5.ml
@@ -1,9 +1,13 @@
 (* Copyright 2015, 2016 Yoann Padioleau, see copyright.txt *)
+open Common
 
 let parse file =
   file |> Common.with_file_in (fun chan ->
     Globals.line := 1;
     let lexbuf = Lexing.from_channel chan in
-    Parser_asm5.program Lexer_asm5.token lexbuf
+    try 
+      Parser_asm5.program Lexer_asm5.token lexbuf
+    with Parsing.Parse_error ->
+      failwith (spf "Syntax error: line %d" !Globals.line)
   )
 

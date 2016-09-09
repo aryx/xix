49415d2f1edf779ff990f8d5eaae77656d3cea44
pad <yoann.padioleau@gmail.com>
2016-09-09 11:16:54 -0700

    * shell/compile.ml: estet

--- shell/compile.ml
+++ shell/compile.ml
@@ -6,6 +6,8 @@ module O = Opcode
 (*****************************************************************************)
 (* Prelude *)
 (*****************************************************************************)
+(* AST to opcodes.
+*)
 
 (*****************************************************************************)
 (* Helpers *)
@@ -22,23 +24,21 @@ let rec split_at_non_assign = function
 (* Compilation algorithm *)
 (*****************************************************************************)
 
-(* todo: need pass eflag in all the subfunc below? 
- * maybe now that all types are mutually recursive.
- *)
 let outcode_seq seq eflag (emit,set,idx) =
 
   let rec xseq seq eflag =
-   (* less set iflast *)
+   (* less set iflast, for if not checking *)
     seq |> List.iter (fun x -> xcmd x eflag)
   
   and xcmd cmd eflag =
     match cmd with
     | A.Simple (w, ws) -> 
         emit (O.F O.Mark);
-        xwords ws eflag;
-        xword w eflag;
+        xwords ws;
+        xword w;
         emit (O.F O.Simple);
-        if eflag then emit (O.F O.Eflag);
+        if eflag 
+        then emit (O.F O.Eflag);
 
     | A.Assign (val1, val2, cmd) ->
         let all_assigns, cmd = 
@@ -48,9 +48,9 @@ let outcode_seq seq eflag (emit,set,idx) =
         | A.EmptyCommand -> 
             all_assigns |> List.iter (fun (val1, val2) ->
               emit (O.F O.Mark);
-              xword val2 eflag;
+              xword val2;
               emit (O.F O.Mark);
-              xword val1 eflag;
+              xword val1;
               emit (O.F O.Assign);
             )
 
@@ -58,9 +58,9 @@ let outcode_seq seq eflag (emit,set,idx) =
         | _ -> 
             all_assigns |> List.iter (fun (val1, val2) ->
               emit (O.F O.Mark);
-              xword val2 eflag;
+              xword val2;
               emit (O.F O.Mark);
-              xword val1 eflag;
+              xword val1;
               emit (O.F O.Local);
             );
             xcmd cmd eflag;
@@ -70,40 +70,47 @@ let outcode_seq seq eflag (emit,set,idx) =
         )
     | A.Pipe (cmd1, cmd2) ->
         emit (O.F O.Pipe);
-        emit (O.I 1);
-        emit (O.I 0);
+        emit (O.I 1); (* left fd *)
+        emit (O.I 0); (* right fd *)
 
         let p = !idx in
         emit (O.I 0);
         let q = !idx in
         emit (O.I 0);
 
+        (* will be executed in a forked child *)
         xcmd cmd1 eflag;
         emit (O.F O.Exit);
 
+        (* will be executed in a children thread *)
         set p (O.I !idx);
         xcmd cmd2 eflag;
         emit (O.F O.Return);
 
+        (* will be executed by parent once the children thread finished *)
         set q (O.I !idx);
         emit (O.F O.PipeWait);
         
         
-    | _ -> failwith ("TODO: " ^ Dumper.s_of_cmd cmd)
+    | _ -> failwith ("TODO compile: " ^ Dumper.s_of_cmd cmd)
 
-  and xword w eflag =
+ (* Do we need to pass eflag here too?
+  * Even though types are mutually recursive because of Backquote, the
+  * compilation of backquote does not use eflag!
+  *)
+  and xword w =
     match w with
     | A.Word (s, _quoted) ->
         emit (O.F O.Word);
         emit (O.S s);
 
     | A.List ws ->
-        xwords ws eflag
+        xwords ws
 
-    | _ -> failwith ("TODO: " ^ Dumper.s_of_value w)
+    | _ -> failwith ("TODO compile: " ^ Dumper.s_of_value w)
 
-  and xwords ws eflag =
-    ws |> List.rev |> List.iter (fun w -> xword w eflag);
+  and xwords ws =
+    ws |> List.rev |> List.iter (fun w -> xword w);
     
   in
   xseq seq eflag
@@ -135,6 +142,7 @@ let compile seq =
   let set idx2 x =
     if idx2 < 0 || idx2 >= !len_codebuf
     then failwith (spf "Bad address %d in set()" idx2);
+
     !codebuf.(idx2) <- x;
   in
   
@@ -143,5 +151,6 @@ let compile seq =
   (* less: O.F O.End *)
   (* less: heredoc, readhere() *)
 
+  (* return the trimmed array *)
   Array.sub !codebuf 0 !idx
   |> (fun x -> if !Flags.dump_opcodes then pr2 (Dumper.s_of_codevec x); x)

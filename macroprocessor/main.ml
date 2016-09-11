caeb356138e158633c0591e0a3a2732012781d33
pad <yoann.padioleau@gmail.com>
2016-09-11 10:03:39 -0700

    * macroprocessor/main.ml: todo

---
+++ macroprocessor/main.ml
@@ -0,0 +1,6 @@
+(* 
+ * todo: 
+ *   - make it an independent program but also a library
+ *     so can avoid some forks! How integrate with lexer of 5a and 5c?
+ *     generate wrapper getchar for Lexing? Can?
+ *)

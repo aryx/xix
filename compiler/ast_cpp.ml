0d3ffe66d9facfdbfe41acba4b1d5c49c35eb35d
pad <yoann.padioleau@gmail.com>
2016-09-23 14:08:56 -0700

    * compiler/ast_cpp.ml:

---
+++ compiler/ast_cpp.ml
@@ -0,0 +1,31 @@
+(* Copyright 2016 Yoann Padioleau, see copyright.txt *)
+
+(*****************************************************************************)
+(* Prelude *)
+(*****************************************************************************)
+(* An Abstract Syntax Tree (AST) for cpp directives.
+*)
+
+(*****************************************************************************)
+(* The AST related types *)
+(*****************************************************************************)
+
+type directive =
+  | Include of Common.filename * bool (* true if <>, false if "" *)
+
+  | Line of int * Common.filename
+
+  (* the define body has been processed and every parameter occurence
+   * is replaced by a #xxx where xxx is the number corresponding to the
+   * ith parameter.
+   * Note that you can have Define (,None,) and Define (,Some ([],false),)
+   *)
+  | Define of string * (string list * bool (* ... *)) option * string option
+  | Undef of string
+
+  | Ifdef of string
+  | Ifndef of string
+  | Else
+  | Endif
+
+  | Pragma of string * string

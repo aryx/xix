3e0ffe751b24bb4690b8e6d9008f2bd8d1c97f37
pad <yoann.padioleau@gmail.com>
2016-11-30 17:38:37 -0800

    * compiler/tests/redeclaration.c:

---
+++ compiler/tests/redeclaration.c
@@ -0,0 +1,27 @@
+int x;
+int x;
+
+int foo(int x, int x) {
+  return x;
+}
+
+int bar() {
+  int x;
+  int x;
+  return x;
+}
+
+struct X {
+  int x;
+};
+
+struct X {
+  int y;
+};
+
+
+struct Y {
+  struct Y {
+    int z;
+  };
+};

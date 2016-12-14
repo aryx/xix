543126e75c46ba31db40faf46eb9f87c2c3b633e
pad <yoann.padioleau@gmail.com>
2016-12-13 22:50:34 -0800

    * compiler/codegen5.ml: skeleton expr

--- compiler/codegen5.ml
+++ compiler/codegen5.ml
@@ -15,7 +15,6 @@ module TC = Typecheck
 (*****************************************************************************)
 (*
  * todo:
- *   - compute offset for local/params
  *   - firstarg opti
  *   - structure alignment (sualign)
  *)
@@ -44,6 +43,19 @@ type env = {
   offsets: (Ast.fullname, int) Hashtbl.t;
 }
 
+let rRET = A.R 0
+let rARG = A.R 0
+
+(* less:
+type addrable = 
+ | Const 
+ | Name
+ | Register
+ | IndReg
+ | Addr
+ | Ind
+ | Add
+*)
 
 (*****************************************************************************)
 (* Helpers *)
@@ -96,18 +108,99 @@ let entity_of_id fullname idinfo =
     A.signature = None;
   }
 
+(*****************************************************************************)
+(* Register allocation helpers *)
+(*****************************************************************************)
+(*
+let reguse env reg
+let regfree env reg
+let regalloc env e_orig dst_regopt
+
+*)
+
+(*****************************************************************************)
+(* Code generation helpers *)
+(*****************************************************************************)
+
+let gopcode env =
+  raise Todo
+
+let gmove env =
+  raise Todo
 
 (*****************************************************************************)
 (* Expression *)
 (*****************************************************************************)
 
+(* todo: inrel ? 
+ * todo: if complex type node
+ *)
+let rec expr env e0 optdst =
+
+  (* todo: if addrable, then gmove *)
+
+  match e0.e with
+  | Sequence (e1, e2) -> 
+    expr env e1 None;
+    expr env e2 optdst
+
+  (* less: lots of possible opti *)
+  | Binary (e1, op, e2) ->
+    (match op with
+    | Arith (Plus | Minus 
+            | And | Or | Xor 
+            | ShiftLeft | ShiftRight
+            | Mul | Div | Mod) ->
+      raise Todo
+    | Logical _ ->
+      raise Todo
+    )
+
+  | Assign (op, e1, e2) ->
+    (match op with
+    | SimpleAssign ->
+      raise Todo
+    | OpAssign op ->
+      raise Todo
+    )
+
+  | _ -> 
+    pr2 (Dumper.s_of_any (Expr e0));
+    raise Todo
+
 (*****************************************************************************)
 (* Statement *)
 (*****************************************************************************)
 let rec stmt env st0 =
-  if true
-  then ()
-  else ()
+  match st0.s with
+  | ExprSt e -> expr env e None
+  | Block xs -> xs |> List.iter (stmt env)
+  | Var { v_name = fullname; } ->
+      let idinfo = Hashtbl.find env.ids fullname in
+      (* todo: generate code for idinfo.ini *)
+
+      (* update env.offsets *)
+      let t = idinfo.TC.typ in
+      let sizet = env.arch.Arch.width_of_type {Arch.structs = env.structs} t in
+      (* todo: align *)
+      Hashtbl.add env.offsets fullname env.offset_locals;
+      env.offset_locals <- env.offset_locals + sizet;
+  | Return eopt ->
+    (match eopt with
+    | None ->
+      add_instr env (A.Instr (A.RET, A.AL)) st0.s_loc
+    | Some e ->
+      (* todo: if type compatible with R0 *)
+      (* todo: reguse, regfree, with_reg *)
+      let dst = rRET in
+      expr env e (Some dst);
+      add_instr env (A.Instr (A.RET, A.AL)) st0.s_loc
+    )
+    
+  | _ -> 
+    pr2 (Dumper.s_of_any (Stmt st0));
+    raise Todo
+
 
 (*****************************************************************************)
 (* Main entry point *)
@@ -134,17 +227,37 @@ let codegen (ids, structs, funcs) =
     offsets = Hashtbl.create 0;
   } in
 
-  funcs |> List.iter (fun { f_name=name; f_loc=loc; f_body=st } ->
+  funcs |> List.iter (fun { f_name=name; f_loc=loc; f_body=st; f_type=typ } ->
     let fullname = (name, 0) in
     let idinfo = Hashtbl.find env.ids fullname in
     (* less: can set fields from C? *)
     let attrs = noattr in
 
     let spc = add_fake_instr env "TEXT" in
+    
+    (* set offsets for parameters *)
     let offsets = Hashtbl.create 11 in
-    (* todo: add offsets for paramters
-       todo: align offset_locals with return type
-    *)
+    let (_typret, (typparams, _varargs)) = typ in
+    let t = idinfo.TC.typ in
+    let tparams = 
+      match t with
+      | T.Func (_tret, tparams, _varargs) -> tparams
+      | _ -> raise (Impossible "not a FUNC")
+    in
+    assert (List.length tparams = List.length typparams);
+    let xs = Common2.zip typparams tparams in
+    let offset = ref 0 in
+    xs |> List.iter (fun (p, t) ->
+      let sizet = env.arch.Arch.width_of_type {Arch.structs = env.structs} t in
+      p.p_name |> Common.if_some (fun fullname ->
+        Hashtbl.add offsets fullname !offset
+      );
+      (* todo: align *)
+      offset := !offset + sizet;
+    );
+    
+    
+    (* todo: align offset_locals with return type *)
     let newenv = { env with 
       size_locals = 0;
       offset_locals = 0;
@@ -164,6 +277,3 @@ let codegen (ids, structs, funcs) =
 
   (Array.sub env.code 0 (env.pc - 1) |> Array.to_list) @
   List.rev env.data
-
-
-  

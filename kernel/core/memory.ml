2de9bc461022093ec343a24a89bc2c1a6439b38a
pad <yoann.padioleau@gmail.com>
2017-04-12 11:47:46 -0700

    * kernel/core/memory.ml: adjust for raspi2

--- kernel/core/memory.ml
+++ kernel/core/memory.ml
@@ -17,7 +17,7 @@ let utzero = V (0x0 + pg2by, User)
 (* memory-mapped IO *)
 
 (* less: raspberry-specific *)
-let physIO = P (0x20000000)
+let physIO = P (0x3F000000) (* raspi1 = 0x20000000 *)
 let virtIO = V (0x3E000000, Kernel) (* TODO: should be 0x7E000000 *)
 
 

268ad00e267b60c73c20a1b8b997cddfba280927
pad <yoann.padioleau@gmail.com>
2017-04-13 08:31:28 -0700

    * kernel/Include/: deleted many header files not used

--- kernel/Include/marshal.h
+++
@@ -1,7 +0,0 @@
-#define	BGSHORT(p)		(((p)[0]<<0) | ((p)[1]<<8))
-#define	BGLONG(p)		((BGSHORT(p)<<0) | (BGSHORT(p+2)<<16))
-#define	BPSHORT(p, v)		((p)[0]=(v), (p)[1]=((v)>>8))
-#define	BPLONG(p, v)		(BPSHORT(p, (v)), BPSHORT(p+2, (v)>>16))
-
-extern byte*	bufimage(Display*, int);
-

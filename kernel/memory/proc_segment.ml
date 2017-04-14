9249e98246f5a8550d697d4c3f991db4e6ccfed9
pad <yoann.padioleau@gmail.com>
2017-04-13 17:52:56 -0700

    * kernel/memory/proc_segment.ml: v1

---
+++ kernel/memory/proc_segment.ml
@@ -0,0 +1,17 @@
+open Types
+
+exception Found of Segment_.t
+
+(* less: dolock bool *)
+let segment_of_addr p addr =
+  try 
+    p.Proc_.seg |> Hashtbl.iter (fun _section seg ->
+        let (VU va) = addr in
+        let (VU base) = seg.Segment_.base in
+        let (VU top) = seg.Segment_.top in
+        if va >= base && va < top
+        then raise (Found seg)
+    );
+    raise Not_found
+  with Found x -> x
+  

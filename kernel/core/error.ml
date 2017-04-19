c6762b136e91f493874b7f715e9397967f440ec1
pad <yoann.padioleau@gmail.com>
2017-04-19 16:52:10 -0700

    * kernel/processes/sysexits.ml: start

--- kernel/core/error.ml
+++ kernel/core/error.ml
@@ -1,6 +1,11 @@
+open Common
+
 (* less: use Error of error? as in ocaml source? easier for pretty printing *)
 exception Ebadarg
 exception Enovmem
 exception Esoverlap
 exception Enochild
 exception Ebadexec
+
+let panic str =
+  raise (Impossible ("panic: " ^ str))
--- kernel/processes/sysexits.ml
+++ kernel/processes/sysexits.ml
@@ -2,5 +2,25 @@ open Common
 open Types
 open Proc_
 
+(* in C the str can be null pointer but better use empty string *)
 let syscall_exits str =
-  raise Todo
+
+  let up = !Globals.up in
+  (* in C it looks if parent is nil, but I use only parentpid
+   * and so parent is nil can also mean RFNOWAIT so I instead
+   * look if pid is 1 to check if boot process.
+   *)
+  if up.pid = 1
+  then Error.panic ("boot process died: " ^ str);
+
+  up.parent |> Common.if_some (fun parent ->
+    raise Todo
+  );
+
+  (* todo: why need that?? *)
+  Spinlock.lock Proc.allocator.Proc.l;
+  Spinlock.lock Page.allocator.Page.l;
+
+  up.state <- Proc_.Moribund;
+  !Hooks.Scheduler.sched ();
+  Error.panic "syscall_exits: should never each this point";

41b22cc35892de71991e0d9685ae6b33a5a87fcf
pad <yoann.padioleau@gmail.com>
2017-04-26 10:02:47 -0700

    * kernel/Bcm/misc.s: mv important function in time_raspi2.s

--- kernel/Bcm/misc.s
+++ kernel/Bcm/misc.s
@@ -7,7 +7,22 @@
 #define MRRC(coproc, op, rd, rn, crm) \
 	WORD $(0xec500000|(rn)<<16|(rd)<<12|(coproc)<<8|(op)<<4|(crm))
 
-TEXT tmrget(SB), 1, $-4             /* local generic timer physical counter value */
-     MRRC(CpSC, 0, 1, 2, CpTIMER)
+TEXT tmrcmpget(SB), 1, $-4
+     MRRC(CpSC, 2, 1, 2, CpTIMER)
      MOVM.IA [R1-R2], (R0)
      RET
+
+TEXT tmrcmpset(SB), 1, $-4
+     MOVW $0, R2
+     MOVW $(400 * 1000 * 1000), R1
+     MCRR(CpSC, 2, 1, 2, CpTIMER)
+     RET
+
+        
+TEXT xxxset(SB), 1, $-4
+    MOVW $(0x1), R0            
+	MCR CpSC, 0, R0, C(14), C(2), 1
+
+TEXT xxxget(SB), 1, $-4
+	MRC CpSC, 0, R0, C(14), C(2), 1
+                                

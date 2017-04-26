6c48d1724ebbf86288d30cd950eeefacd3d0cca4
pad <yoann.padioleau@gmail.com>
2017-04-26 10:02:00 -0700

    * kernel/test.c: test the timer interrupt

--- kernel/test.c
+++ kernel/test.c
@@ -5,7 +5,6 @@
 #include "dat.h"
 #include "fns.h"
 
-// temp
 #include "arm.h"
 
 #define ARMLOCAL    (VIRTIO+IOSIZE)
@@ -13,34 +12,85 @@
 enum {
     Localctl    = 0x00,
     Prescaler   = 0x08,
+
+    /* generic timer (cortex-a7) */
+    Enable  = 1<<0,
+    Imask   = 1<<1,
+    Istatus = 1<<2,
 };
 
 extern uvlong tmrget(void);
+extern uvlong tmrcmpget(void);
+extern void   tmrcmpset(void);
+
+extern void   xxxset(void);
+extern int   xxxget(void);
 
 void test(void) {
+    uvlong v;
+    int x;
+	//volatile u32int *addr;
+    //addr = (volatile u32int *) (ARMLOCAL + 0x0040); 
+    //*addr = 0x1; // enable CNTPSIRQ
 
-    *(ulong*)(ARMLOCAL + Localctl) = 0;             /* magic */
-    *(ulong*)(ARMLOCAL + Prescaler) = 0x06aaaaab;   /* magic for 1 Mhz */
+    x = cprdsc(0, CpTIMER, 0, 0);
+    print("freq = %d\n", x);
 
-    ulong x, y;
+    v = tmrget();
+    print("val1: %lld\n", v);
 
-    x = *(ulong*)(ARMLOCAL + 0x1C);
-    y = *(ulong*)(ARMLOCAL + 0x20);
-    print("%d %d\n", x, y); // does not work, get 0 0
+    cpwrsc(0, CpTIMER, CpTIMERphys, CpTIMERphysval, 62500000);
+    cpwrsc(0, CpTIMER, CpTIMERphys, CpTIMERphysctl, Enable);
+    
+    //xxxset();
+    //print("val = %d", xxxget());
+
+    //assert(arch_islo());
+    arch_spllo();
+}
+
+void test3(void) {
+    uvlong v;
+    unsigned int x;
+
+    v = tmrget();
+    print("val1: %lld\n", v);
+    while (v < 200 * 1000 * 1000) {
+      v = tmrget();
+    }
+    v = tmrget();
+    print("val2: %lld\n", v);
+
+    tmrcmpset();
+
+    v = tmrcmpget();
+    print("cmp: %lld\n", v);
+
+
+    //x = (unsigned int) cprdsc(0, CpTIMER, CpTIMERphys, CpTIMERphysval);
+    //print("val1: %d\n", x);
+    //x = cprdsc(0, CpTIMER, CpTIMERphys, CpTIMERphysval);
+    //print("val2: %d\n", x);
+    //
+    cpwrsc(0, CpTIMER, CpTIMERphys, CpTIMERphysval, 5000);
+    cpwrsc(0, CpTIMER, CpTIMERphys, CpTIMERphysctl, Enable);
+
 
 }
 
 void test2(void) {
-    ulong x;
+    ulong x, y;
     uvlong v;
 
-    x = cprdsc(0, CpTIMER, 0, 0);
-    print("freq = %d\n", x);
 
     *(ulong*)(ARMLOCAL + Localctl) = 0;             /* magic */
     *(ulong*)(ARMLOCAL + Prescaler) = 0x06aaaaab;   /* magic for 1 Mhz */
 
     arch_coherence();
+
+    x = *(ulong*)(ARMLOCAL + 0x1C);
+    y = *(ulong*)(ARMLOCAL + 0x20);
+    print("%d %d\n", x, y); // does not work, get 0 0
 
     // cpwrsc(0, CpTIMER, 0, 0, 1000000); // does not change anything
 

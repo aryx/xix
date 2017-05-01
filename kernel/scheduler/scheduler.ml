b5a64c4b96f4a9527edd8ddf880c17d42be8d577
pad <yoann.padioleau@gmail.com>
2017-05-01 14:46:21 -0700

    * kernel/scheduler/scheduler.ml: find_proc and start of sched/schedinit

--- kernel/scheduler/scheduler.ml
+++ kernel/scheduler/scheduler.ml
@@ -8,40 +8,58 @@ type runq = {
   l: Spinlock_.t;
   (* bitset *)
   mutable runvec: int;
-  mutable nb_ready: int;
+  mutable nready: int;
 }
 
 let runq = {
   queues = Array.init nb_priorities (fun _i -> Queue.create () );
   l = Spinlock.alloc();
   runvec = 0;
-  nb_ready = 0;
+  nready = 0;
 }
 
 let any_ready () =
   runq.runvec <> 0
 
-let add p = 
+(* the priority of a process must be regularly adjusted (to avoid starving) *)
+let add p prio = 
   Spinlock.lock runq.l;
 
-  (* less: priority can be an argument instead of constant *)
-  let (Prio iprio) = p.Proc_.priority in
+  p.Proc_.priority <- prio;
+  let (Prio iprio) = prio in
   
   Queue.add p runq.queues.(iprio);
-  runq.nb_ready <- runq.nb_ready + 1;
+  runq.nready <- runq.nready + 1;
   runq.runvec <- runq.runvec lor (1 lsl iprio);
 
   Spinlock.unlock runq.l
 
-let dequeue p =
-  raise Todo
+(* less: take process p as a parameter and check if still in q *)
+let dequeue prio =
+  (* less: do the canlock, return nil, and loop? or assume caller has lock *)
+  if Spinlock.canlock runq.l
+  then failwith "Caller should hold runq.l";
+  (*less: Spinlock.lock runq.l;*)
+  let (Prio iprio) = prio in
+  let p = Queue.take runq.queues.(iprio) in
+  if p.Proc_.state <> Proc_.Ready
+  then failwith (spf "process %s %d in runq was not in Ready state" 
+                   p.Proc_.name p.Proc_.pid);
+  
+  if Queue.length runq.queues.(iprio) = 0
+  then runq.runvec <- runq.runvec land (lnot (1 lsl iprio));
+  runq.nready <- runq.nready - 1;
+  (*less: Spinlock.unlock runq.l;*)
+  p
 
-exception Found of Proc_.t
+    
+
+exception Found of Scheduler_.priority
 
 let find_proc () =
   (* todo: if cpu->readied *)
   (* less: use Proc.lastcpu and affinity *)
-  (* todo: spllo()? *)
+  (* less: spllo()/splhi() dance? improve concurrency? *)
 
   (* less: do not lock runq? do it in 2 steps in a loop? *)
   Spinlock.lock runq.l;
@@ -50,24 +68,46 @@ let find_proc () =
     while true do 
       for i = Scheduler_.nb_priorities -1 to 0 do
         if Queue.length runq.queues.(i) > 0
-        then raise (Found (Queue.take runq.queues.(i)))
+        then raise (Found (Scheduler_.Prio i))
       done;
       (* nothing found?? *)
       Spinlock.unlock runq.l;
       failwith "todo: idlehands"
     done;
     raise (Impossible "while infinite loop can exit only through raise")
-  with Found p ->
-    (* todo: splhi? *)
+  with Found prio ->
+    (* less: splhi? *)
+    let p = dequeue prio in
+    (* less: loop and dance if runq has changed in between *)
     p.Proc_.state <- Proc_.Scheding;
     (* less: lastcpu *)
+    (* less: proctrace strace *)
+    Spinlock.unlock runq.l;
     p
 
 (* the big one! *)
 let sched () =
+  (* todo: check ilockdepth  *)
   raise Todo
 
-let schedinit () =
+let scheduler () =
+  (* less: assert splhi? *)
+  (* less: check ilockdepth  *)
+  (* todo: handle Moribund *)
+  (* todo: handle Ready? *)
+
+  (* from now on, up is nil *)
+
+  let p = find_proc () in
+  (* less: update priority *)
+  (* less: adjust schedticks *)
+  
+  Globals.cpu.Cpu.proc <- Some p;
+  let up = Globals.up () in
+  up.Proc_.state <- Proc_.Running;
+  (* less: up.Proc_.cpu <- cpu.id *)
+
+  (* todo: mmuswitch *)
   raise Todo
 
 

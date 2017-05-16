b354929df59d6ecd9dc3e23d8c0389ae44456d34
pad <yoann.padioleau@gmail.com>
2017-05-16 09:45:35 -0700

    * windows/main.ml: start Arg

--- windows/main.ml
+++ windows/main.ml
@@ -1,6 +1,29 @@
+open Common
+
+let usage = 
+  "usage: rio [options]"
+
+let thread_main () =
+  raise Todo
 
 let main () =
+  (*
   Test.test ()
+  *)
+  let options = (*todo: Arg.align*) [
+    "-s", Arg.Unit (fun () -> raise Todo),
+    " ";
+    "-font", Arg.String (fun s -> raise Todo),
+    " <fontname>";
+    "-i", Arg.String (fun s -> raise Todo),
+    " <initcmd>";
+    "-k", Arg.String (fun s -> raise Todo),
+    " <kbdcmd>";
+  ]
+  in
+  Arg.parse options (fun _ -> Arg.usage options usage) usage;
+  thread_main ()
+  
 
 let _ =
   main ()

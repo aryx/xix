1ca378270eabc32ee7dcaadba13985a343caf664
pad <yoann.padioleau@gmail.com>
2017-05-16 17:20:04 -0700

    * windows/fs.ml: create pipe

--- windows/fs.ml
+++ windows/fs.ml
@@ -1,5 +1,11 @@
 open Common
 
+module Unix2 = ThreadUnix
+  
+(* Note that pipes created under plan9 are bidirectional! 
+ * No need to create 2 pipes for 2-way communication.
+ *)
+
 type t = {
   (* the pipe *)
 
@@ -15,8 +21,19 @@ type t = {
 
 let init () =
   (* less: close on exec flag, or need to do it when exec in winshell *)
-  let (pipe_for_read, pipe_for_write) = Unix.pipe () in
+  let (fd1, fd2) = Unix2.pipe () in
+  (* todo: default threadUnix implementation just set non_block for fd2, but
+   * the in_fd, but in plan9 pipes are bidirectional so need non_block for
+   * both file descriptors.
+   *)
+  Unix.set_nonblock fd1;
+
   
   (* less: let user = Common.cat "/dev/user" |> String.concat "" in *)
   
-  raise Todo
+  { clients_fd = fd1;
+    server_fd = fd2;
+
+    user = "pad";
+    fids = Hashtbl.create 101;
+  }

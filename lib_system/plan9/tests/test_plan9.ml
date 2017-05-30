bdcd215f8f28ee6adaf8a865ba02e1975cf4c243
pad <yoann.padioleau@gmail.com>
2017-05-29 17:34:48 -0700

    * lib_system/plan9/tests/test_plan9.ml: list all dir to test

--- lib_system/plan9/tests/test_plan9.ml
+++ lib_system/plan9/tests/test_plan9.ml
@@ -1,9 +1,22 @@
 open Common
 
 let main () =
-  let res = Plan9.bind "/tests/xxx" "/" Plan9.mAFTER in
+  let res = Plan9.bind "/tests/xxx" "/" Plan9.mREPL in
+  (*
   let file = Unix.openfile "/test_plan9" [Unix.O_RDWR] 0o666 in
   Unix.close file
+  *)
+  let dir = Unix.opendir "/" in
+  
+  let rec aux () =
+    try 
+      let str = Unix.readdir dir in
+      pr str;
+      aux ()
+    with End_of_file -> ()
+  in
+  aux ();
+  Unix.closedir dir
 
 
 let _ =

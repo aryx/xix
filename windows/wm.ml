fa525aa583286de73796f0e58d80d54da26a6b63
pad <yoann.padioleau@gmail.com>
2017-06-02 16:13:25 -0700

    * windows/processes_winshell.ml: mv stuff from wm here

--- windows/wm.ml
+++ windows/wm.ml
@@ -136,21 +136,7 @@ let new_win img cmd argv pwd_opt mouse fs =
   | -1 -> failwith "fork returned -1"
   | 0 ->
     (* child *)
-    Unix1.chdir w.W.pwd;
-    (* todo: close on exec *)
-    (* less: rfork for copy of namespace/fd/env, but ape fork does that? *)
-    
-    (*/* close server end so mount won't hang if exiting */*)
-    Unix1.close fs.FS.server_fd;
-    Plan9.mount fs.FS.clients_fd (-1) "/mnt/wsys" Plan9.MRepl (spf "%d" w.W.id);
-    Plan9.bind "/mnt/wsys" "/dev" Plan9.MBefore;
-
-    (* less: wclose for ref counting *)
-    (* todo: reassign STDIN/STDOUT *)
-    (* less: notify nil *)
-    Unix2.execv cmd argv;
-    failwith "exec failed"
-
+    Processes_winshell.child_of_fork w fs cmd argv
   | pid -> 
     (* parent *)
     Thread.critical_section := false;

4f2b238f682153a2db839a1070c31e97de8a18cc
pad <yoann.padioleau@gmail.com>
2017-06-12 19:27:51 -0700

    * windows/processes_winshell.ml: * windows/processes_winshell.ml: bugfix: forgot perm to openfile argument!

--- windows/processes_winshell.ml
+++ windows/processes_winshell.ml
@@ -21,13 +21,22 @@ let run_cmd_in_window_in_child_of_fork cmd argv w fs =
   (* less: wclose for ref counting *)
   (* todo: handle errors? Unix_error? then communicate failure to parent? *)
   Unix1.close Unix1.stdin;
-  let _ = Unix1.openfile "/dev/cons" [Unix1.O_RDONLY] in
+  let fd = Unix1.openfile "/dev/cons" [Unix1.O_RDONLY] 0o666 in
+  if fd <> Unix1.stdin
+  then failwith "could not reassign stdin";
 
-(* TODO
   Unix1.close Unix1.stdout;
-  let _ = Unix1.openfile "/dev/cons" [Unix1.O_WRONLY] in
-  Unix1.dup2 Unix1.stdout Unix1.stderr;
-*)
+  let fd = Unix1.openfile "/dev/cons" [Unix1.O_WRONLY] 0o666 in
+  if fd <> Unix1.stdout
+  then failwith "could not reassign stdout";
+  
+  (* todo: Unix1.dup2 Unix1.stdout Unix1.stderr; 
+   * this creates a kernel crash! 'double sleep' error.
+  *)
+(*
+  Unix1.close Unix1.stderr;
+  let _ = Unix1.openfile "/dev/cons" [Unix1.O_WRONLY] 0o666 in
+*)  
 
   (* less: notify nil *)
   Unix2.execv cmd argv;

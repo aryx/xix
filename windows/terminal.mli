2575be13f93a24f39d33af3fd9e4bdfb312347df
pad <yoann.padioleau@gmail.com>
2017-06-28 17:00:04 -0700

    * windows/terminal.mli:

---
+++ windows/terminal.mli
@@ -0,0 +1,39 @@
+type position = {
+  i: int;
+}
+
+type t = {
+  (* the model *)
+
+  mutable text: Rune.t array;
+  mutable nrunes: int;
+
+  mutable cursor: position;
+  mutable end_selection: position option;      (* old: q1 in rio-C) *)
+
+  mutable output_point: position;
+
+  (* the view *)
+
+  img: Image.t;
+
+  r: Rectangle.t;
+  textr: Rectangle.t;
+  scrollr: Rectangle.t;
+
+  mutable origin_visible: position;
+  mutable runes_visible: int;
+
+  font: Font.t;
+
+  mutable is_selected: bool;
+}
+
+val alloc: Image.t -> Font.t -> t
+
+val key_in: t -> Keyboard.key -> unit
+val runes_in: t -> Rune.t list -> unit
+
+val newline_after_output_point: t -> bool
+
+val repaint: t -> unit

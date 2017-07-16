da41dda4378a46a6a60c76b804cb6fac2671c566
pad <yoann.padioleau@gmail.com>
2017-07-16 06:41:05 -0700

    * commons/common.mli:

---
+++ commons/common.mli
@@ -0,0 +1,72 @@
+
+type byte = char
+type bytes = string
+
+type filename = string
+
+type ('a, 'b) either = Left of 'a | Right of 'b
+
+val spf : ('a, unit, string) format -> 'a
+val pr : string -> unit
+val pr2 : string -> unit
+
+val with_file_out : (out_channel -> 'a) -> string -> 'a
+val with_file_in : (in_channel -> 'a) -> string -> 'a
+
+val push : 'a -> 'a list ref -> unit
+
+exception Todo
+exception Impossible of string
+
+val rnd : int -> int -> int
+
+val if_some : ('a -> unit) -> 'a option -> unit
+val filter_some : 'a option list -> 'a list
+val map_filter : ('a -> 'b option) -> 'a list -> 'b list
+
+val sort_by_val_highfirst : ('a * 'b) list -> ('a * 'b) list
+val sort_by_val_lowfirst : ('a * 'b) list -> ('a * 'b) list
+val sort_by_key_highfirst : ('a * 'b) list -> ('a * 'b) list
+val sort_by_key_lowfirst : ('a * 'b) list -> ('a * 'b) list
+
+val group_by : ('a -> 'b) -> 'a list -> ('b * 'a list) list
+
+val memoized :
+  ?use_cache:bool -> ('a, 'b) Hashtbl.t -> 'a -> (unit -> 'b) -> 'b
+
+val cat : string -> string list
+
+module Regexp_ :
+  sig
+    val matched : int -> string -> string
+    val matched1 : string -> string
+    val matched2 : string -> string * string
+    val matched3 : string -> string * string * string
+    val matched4 : string -> string * string * string * string
+    val matched5 : string -> string * string * string * string * string
+    val matched6 :
+      string -> string * string * string * string * string * string
+    val matched7 :
+      string -> string * string * string * string * string * string * string
+    val _memo_compiled_regexp : (string, Str.regexp) Hashtbl.t
+    val candidate_match_func : string -> string -> bool
+    val split : string -> string -> string list
+  end
+val ( =~ ) : string -> string -> bool
+
+module List_ :
+  sig
+    val exclude : ('a -> bool) -> 'a list -> 'a list
+    val take : int -> 'a list -> 'a list
+    val take_safe : int -> 'a list -> 'a list
+  end
+
+module Hashtbl_ :
+  sig
+    val of_list : ('a * 'b) list -> ('a, 'b) Hashtbl.t
+    val to_list : ('a, 'b) Hashtbl.t -> ('a * 'b) list
+  end
+
+module Obj_ : sig val dump2 : Obj.t -> string end
+val dump : 'a -> string
+val pr2_gen : 'a -> unit

7a77514fd8250b47bf27e0685a97c4593d4326b0
pad <yoann.padioleau@gmail.com>
2017-07-19 11:55:42 -0700

    * version_control/refs.ml: better types by splitting in 2

--- version_control/refs.ml
+++ version_control/refs.ml
@@ -1,6 +1,10 @@
-
 type t =
   | Head (* "HEAD" *)
-  | Hash of Commit.hash
-  (* pointer (may contain sha1 or another pointer) *)
+  (* should always start with "refs/" *)
   | Ref of Common.filename (* e.g. "refs/heads/master" *)
+
+type ref_content =
+  (* the final value when follow all the pointers *)
+  | Hash of Commit.hash
+  (* pointer (may contain sha1 or another pointer again) *)
+  | OtherRef of Common.filename
--- version_control/refs.mli
+++ version_control/refs.mli
@@ -1,6 +1,9 @@
 
 type t =
   | Head (* "HEAD" *)
-  | Hash of Commit.hash
-  (* pointer (may contain sha1 or another pointer) *)
   | Ref of Common.filename (* e.g. "refs/heads/master" *)
+
+type ref_content =
+  | Hash of Commit.hash
+  (* pointer (may contain sha1 or another pointer again) *)
+  | OtherRef of Common.filename

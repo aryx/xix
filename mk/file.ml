a4cf150ae76b17f93516ab30b0275ce0ed1415fc
pad <yoann.padioleau@gmail.com>
2018-04-18 12:51:19 -0700

    * mk/file.ml: bugfix use stat not lstat so mk can handle symlinks and correctly recompile stuff under plan9/COMPILE/9/pc

--- mk/file.ml
+++ mk/file.ml
@@ -5,7 +5,10 @@ open Common
 (* opti? time cache, and flag to skip cache if want refresh *)
 let timeof file =
   try 
-    let stat = Unix.lstat file in
+    (* bugfix: use stat, not lstat, to get the time of what is pointed
+     * by the symlink, not the symlink
+     *)
+    let stat = Unix.stat file in
     Some (stat.Unix.st_mtime)
   with Unix.Unix_error (_, _, _) -> None
 

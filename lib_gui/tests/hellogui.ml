60ce1bd9a3f923e0e8bb2192f8938a9aa49c739c
pad <yoann.padioleau@gmail.com>
2018-09-04 17:44:08 -0700

    * lib_gui/tests/hellogui.ml: compile again

--- lib_gui/tests/hellogui.ml
+++ lib_gui/tests/hellogui.ml
@@ -1,13 +1,8 @@
 open Common
 
 let main () =
-  let _ = Graphics.open_graph " 500x500" in
+  let display = Draw.init "Hello GUI" in
 
-  Graphics.loop_at_exit 
-    [Graphics.Button_down;Graphics.Button_up;Graphics.Key_pressed]
-    (fun status ->
-      ()
-    )
-
+  raise Todo
 
 let _ = main ()

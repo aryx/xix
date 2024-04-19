7eb28a1dafbee1af0aa7868aec9b17375bcb2861
pad <pad@r2c.dev>
2024-04-19 11:43:00 +0200

    Add test.sh and related GHA check
    test plan:
    ./bootstrap-mk.sh
    ./test.sh
    and wait for green CI Checks

---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# Basic testing
+# LATER: ideally later we could switch to rc instead of sh
+
+# any error should abort the script
+set -e
+# for showing the executed commands (verbose)
+set -x
+
+# Just make sure at least the programs are not segfaulting
+
+./BOOTSTRAP/mk --help
+./BOOTSTRAP/rc --help

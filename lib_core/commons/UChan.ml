4b5a03fab7ec503ad2ea7fbf8cfb2418ffc59b44
pad <pad@r2c.dev>
2025-05-13 14:39:22 +0200

    UChan: use ~finally: syntax so can be skipped by ocaml light

--- lib_core/commons/UChan.ml
+++ lib_core/commons/UChan.ml
@@ -6,13 +6,11 @@ let with_open_in (f : Chan.i -> 'a) (file : Fpath.t) : 'a =
     open_in !!file 
   in
   let ichan : Chan.i = { ic = chan; origin = Chan.File file } in
-  let finally () : unit = close_in chan in
-  Fun.protect ~finally (fun () -> f ichan)
+  Fun.protect ~finally:(fun () -> close_in chan) (fun () -> f ichan)
 
 let with_open_out (f : Chan.o -> 'a) (file : Fpath.t) : 'a =
   let chan : out_channel =
     open_out !!file 
   in
   let ochan : Chan.o = { oc = chan; p = file } in
-  let finally () = close_out chan in
-  Fun.protect ~finally (fun () -> f ochan)
+  Fun.protect ~finally:(fun () -> close_out chan) (fun () -> f ochan)

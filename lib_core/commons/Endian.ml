243d207f60fe7aa072b4d301c3bbc1a182fec50f
pad <yoann.padioleau@gmail.com>
2025-11-13 14:45:45 +0100

    Endian: add array_xxx variants

--- lib_core/commons/Endian.ml
+++ lib_core/commons/Endian.ml
@@ -5,21 +5,17 @@ open Common
 (*****************************************************************************)
 (* Little vs Big Endian input/output.
  * See https://en.wikipedia.org/wiki/Endianness
+ *
+ * Note that OCaml 4.08 introduced many endian-related functions in buffer.mli,
+ * bytes.mli, and string.mli. See also Sys.big_endian bool since 4.00.0
  *)
 
 (*****************************************************************************)
-(* Big *)
+(* Helpers *)
 (*****************************************************************************)
 
-type t =
-  | Big (* BE *)
-  | Little (* LE *)
-
-module Big = struct
-
-(* old: was called lput in A_out.ml *)
 (* TODO: use Int32.t *)
-let output_32 (chan : out_channel) (word : int) : unit =
+let split_32 (word : int) : byte * byte * byte * byte =
   if word < 0 && word > 0xffffffff
   then raise (Impossible (spf "should call lput with a uint32 not %d" word));
 
@@ -28,25 +24,48 @@ let output_32 (chan : out_channel) (word : int) : unit =
   let x2 = Char.chr ((word lsr 8) mod 256) in
   let x3 = Char.chr ((word lsr 16) mod 256) in
   let x4 = Char.chr ((word lsr 24) mod 256) in
-  (* big part first; most-significant byte first *)
-  output_char chan x4;
-  output_char chan x3;
-  output_char chan x2;
-  output_char chan x1;
-  ()
+  x1, x2, x3, x4
 
-(* old: was called wput in A_out.ml *)
-let output_16 (chan : out_channel) (word : int) : unit =
+let split_16 (word : int) : byte * byte =
   if word < 0 && word > 0xffff
   then raise (Impossible (spf "should call lput with a uint16 not %d" word));
 
   (* could also use land 0xFF ? *)
   let x1 = Char.chr (word mod 256) in
   let x2 = Char.chr ((word lsr 8) mod 256) in
+  x1, x2
+  
+(*****************************************************************************)
+(* Big *)
+(*****************************************************************************)
+
+type t =
+  (* a.k.a. BE, network byte order *)
+  | Big 
+  (* a.k.a. LE *)
+  | Little
+
+
+module Big = struct
+
+let array_32 (word : int) : byte array =
+  let x1, x2, x3, x4 = split_32 word in
   (* big part first; most-significant byte first *)
-  output_char chan x2;
-  output_char chan x1;
-  ()
+  [| x4; x3; x2; x1 |]
+
+let array_16 (word : int) : byte array =
+  let x1, x2 = split_16 word in
+  (* big part first; most-significant byte first *)
+  [| x2; x1 |]
+  
+
+(* old: was called lput in A_out.ml *)
+let output_32 (chan : out_channel) (word : int) : unit =
+  array_32 word |> Array.iter (output_char chan)
+
+(* old: was called wput in A_out.ml *)
+let output_16 (chan : out_channel) (word : int) : unit =
+  array_16 word |> Array.iter (output_char chan)
 
 end
 
@@ -56,33 +75,22 @@ end
 
 module Little = struct
 
-(* old: was called lputl for little-endian put long in linker/Executable.ml *)
-(* TODO: use Int32.t *)
-let output_32 (chan : out_channel) (word : int) : unit =
-  if word < 0 && word > 0xffffffff
-  then raise (Impossible (spf "should call with a uint32 not %d" word));
-
-  let x1 = Char.chr (word mod 256) in
-  let x2 = Char.chr ((word lsr 8) mod 256) in
-  let x3 = Char.chr ((word lsr 16) mod 256) in
-  let x4 = Char.chr ((word lsr 24) mod 256) in
+let array_32 ( word : int) : byte array =
+  let x1, x2, x3, x4 = split_32 word in
   (* little part first; least-significant byte first *)
-  output_char chan x1;
-  output_char chan x2;
-  output_char chan x3;
-  output_char chan x4;
-  ()
+  [| x1; x2; x3; x4 |]
+
+let array_16 ( word : int) : byte array =
+  let x1, x2 = split_16 word in
+  (* little part first; least-significant byte first *)
+  [| x1; x2 |]
+
+(* old: was called lputl for little-endian put long in linker/Executable.ml *)
+let output_32 (chan : out_channel) (word : int) : unit =
+  array_32 word |> Array.iter (output_char chan)
 
 let output_16 (chan : out_channel) (word : int) : unit =
-  if word < 0 && word > 0xffff
-  then raise (Impossible (spf "should call with a uint16 not %d" word));
-
-  let x1 = Char.chr (word mod 256) in
-  let x2 = Char.chr ((word lsr 8) mod 256) in
-  (* little part first; least-significant byte first *)
-  output_char chan x1;
-  output_char chan x2;
-  ()
+  array_16 word |> Array.iter (output_char chan)
 
 end
 
@@ -90,3 +98,8 @@ let output_functions_of_endian (endian : t) =
   match endian with
   | Big -> Big.output_16, Big.output_32
   | Little -> Little.output_16, Little.output_32
+
+let array_functions_of_endian (endian : t) =
+  match endian with
+  | Big -> Big.array_16, Big.array_32
+  | Little -> Little.array_16, Little.array_32

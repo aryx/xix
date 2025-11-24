0ff2c3886117f5f277bf06d4eac676592bcf0bd7
pad <yoann.padioleau@gmail.com>
2025-11-24 14:34:22 +0100

    Fix CI regressions using xix_remove_open.sh -pp for compiler/mkfile

--- compiler/tests/Test_compiler.ml
+++ compiler/tests/Test_compiler.ml
@@ -1,4 +1,5 @@
 open Common
+open Fpath_.Operators
 open Xix_compiler
 
 let t = Testo.create
@@ -30,6 +31,12 @@ let run_main (caps : <CLI.caps; ..>) (cmd : string) : (Exit.t, string) result =
    )
    ()
 
+let ok_or_fail res =
+  match res with
+  | Ok Exit.OK -> ()
+  | Ok x -> failwith (spf "unexpected exit: %s" (Exit.show x))
+  | Error s -> failwith (spf "unexpected failure: %s" s)
+
 (*****************************************************************************)
 (* Tests *)
 (*****************************************************************************)
@@ -37,13 +44,24 @@ let e2e_tests caps =
   Testo.categorize "e2e" [
     (* TODO: use Exit_code.ml to remove the need for End_of_file catch *)
     t ~checked_output:(Testo.stdxxx ()) "--help" (fun () ->
-        match run_main caps "--help" with
-        | Ok Exit.OK -> ()
-        | Ok x -> failwith (spf "unexpected exit: %s" (Exit.show x))
-        | Error s -> failwith (spf "unexpected failure: %s" s)
-
+        run_main caps "--help" |> ok_or_fail
     )
   ]
+
+let codegen_tests caps =
+  (* LATER: use Glob.glob "*.c" *)
+  let files = [
+      "call.c";
+    ]
+  in
+  let tests = files |> List.map (fun file ->
+      let path = Fpath.v "tests/compiler/codegen" / file in
+      t ~checked_output:(Testo.stdxxx ()) !!path (fun () ->
+        run_main caps (spf "-S %s" !!path) |> ok_or_fail
+      )
+    )
+  in
+  Testo.categorize "codegen" tests
 
 (*****************************************************************************)
 (* The suite *)
@@ -52,5 +70,6 @@ let e2e_tests caps =
 let tests caps =
   Testo.categorize_suites "compiler" [
       e2e_tests caps;
+      codegen_tests caps;
   ]
 

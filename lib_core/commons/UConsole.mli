2bcfd7826f9ae9ee61f10d6ea108c838f558a8ee
pad <yoann.padioleau@gmail.com>
2026-01-12 16:24:19 +0100

    lib_core: reduce UConsole.ml, remain just print to remove

--- lib_core/commons/UConsole.mli
+++ lib_core/commons/UConsole.mli
@@ -1,8 +1,3 @@
-(* See Console.mli for more information
- *
- * DO NOT USE THIS FILE! You should use Console.mli instead.
- *)
-
 val print : string -> unit
-val print_no_nl : string -> unit
-val eprint : string -> unit
+
+
